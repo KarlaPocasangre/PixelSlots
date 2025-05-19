9e5df7347da6ee446096f3caf455a0fa75d6b2ff
Karla Pocasangre <karlahernandezpocasangre@gmail.com>
2025-05-19 16:41:52 -0600

    Pruebas Pantalla de Carga

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import "./App.css";
 import Routes from "./routes/Routes";
+import Loading from "./pages/Loading";
 
 function AppRoutes() {
   return useRoutes(Routes);
 }
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1800);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <Router>
       <AppRoutes />
